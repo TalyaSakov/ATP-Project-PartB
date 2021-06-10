161d80ae81540ecb96f0cd5eb975efb7f1ffbf82
Jonathan.P <yonman2@gmail.com>
2021-06-10 10:46:31 +0300

    Diagonals Fixed

--- src/Model/MyModel.java
+++ src/Model/MyModel.java
@@ -69,28 +69,28 @@ public class MyModel extends Observable implements IModel {
                 if (Diagonal_Verification(5)){
                     rowChar --;
                     colChar ++;
-                break;
                 }
+                break;
             case 6: //UP-LEFT
                 if (Diagonal_Verification(6)){
                     rowChar --;
                     colChar --;
-                break;
                 }
+                break;
 
             case 7: //DOWN-RIGHT
                 if (Diagonal_Verification(7)){
                     rowChar ++;
                     colChar ++;
-                break;
                 }
+                break;
 
             case 8://Down-left
                 if (Diagonal_Verification(8)){
                     rowChar ++;
                     colChar --;
-                    break;
                 }
+                    break;
 
         }
 
@@ -206,16 +206,16 @@ public class MyModel extends Observable implements IModel {
         }
     }
 
-    public boolean Diagonal_Verification(int direction) { //TODO: fix Diagonals
+    public boolean Diagonal_Verification(int direction) {
         return switch (direction) {
             case 5 -> //UP-Right
                     ((rowChar != 0) && (maze.getMaze()[rowChar - 1][colChar+1] != 1) && (colChar != maze.getMaze()[0].length - 1) && (((maze.getMaze()[rowChar][colChar + 1] != 1)) || (maze.getMaze()[rowChar -1][colChar ]!= 1)));
             case 6 -> //UP-LEFT
                     (rowChar != 0) && (maze.getMaze()[0].length != 0) && (maze.getMaze()[rowChar - 1][colChar-1] != 1) && ((maze.getMaze()[rowChar-1][colChar] != 1) || (maze.getMaze()[rowChar][colChar-1] != 1));
             case 7 -> //DOWN-RIGHT
-                    ((rowChar != maze.getMaze().length - 1)  && (colChar != maze.getMaze()[0].length - 1) && ((maze.getMaze()[rowChar + 1][colChar] != 1) || (maze.getMaze()[rowChar][colChar + 1] != 1)));
+                    ((rowChar != maze.getMaze().length - 1)  && (colChar != maze.getMaze()[0].length - 1) && ((maze.getMaze()[rowChar + 1][colChar] != 1) || (maze.getMaze()[rowChar][colChar + 1] != 1)) && (maze.getMaze()[rowChar +1][colChar +1] != 1));
             case 8 ->//Down-left
-                    ((rowChar != maze.getMaze().length - 1)  && (colChar != 0) && ((maze.getMaze()[rowChar][colChar - 1] != 1) || (maze.getMaze()[rowChar + 1][colChar] != 1)));
+                    ((rowChar != maze.getMaze().length - 1)  && (colChar != 0) && ((maze.getMaze()[rowChar][colChar - 1] != 1) || (maze.getMaze()[rowChar + 1][colChar] != 1)) && (maze.getMaze()[rowChar +1][colChar -1] != 1));
             default -> false;
         };
     }
